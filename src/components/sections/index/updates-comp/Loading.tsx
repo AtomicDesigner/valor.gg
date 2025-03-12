9959268fa9a6c281ede8f9a49ce58d30c2a3b430
AtomicDesigner <ifeelcooler@gmail.com>
2025-03-12 16:57:02 +0100

    potentially fixed it hovering down

--- src/components/sections/index/updates-comp/Loading.tsx
+++ src/components/sections/index/updates-comp/Loading.tsx
@@ -6,9 +6,9 @@ let perc: number = 35;
 export default function Hero({ inView, descRef }: { inView: boolean, descRef: any }) {
   return (
     <>
-      <section className="max-w-4xl w-full flex flex-col mx-auto pt-20 md:pt-32 lg:pt-44">
+      <section className="max-w-4xl w-full flex flex-col mx-auto pt-44">
         <motion.div
-          className="sm:p-12 p-6 w-full flex flex-col bg-gradient-to-br from-primary to to-secondary rounded-lg border-1 border-accent shadow-2xl shadow-background"
+          className="relative sm:p-12 p-6 w-full flex flex-col bg-gradient-to-br from-primary to to-secondary rounded-lg border-1 border-accent shadow-2xl shadow-background"
           initial={{ transform: 'translateY(30px)', opacity: 0 }}
           whileInView={{ transform: 'translateY(0px)', opacity: 100 }}
           transition={{ duration: 0.5, delay: 0.1, ease: [0.39, 0.21, 0.12, 0.96], }}
@@ -19,10 +19,13 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
         Next Update Progress:
           </h2>
             <div className="mt-6 w-full bg-gray-700 rounded-full h-4 relative bg-gradient-to-br from-primary to to-secondary rounded-lg border-1 border-accent shadow-2xl shadow-background">
-              <div
-              className="bg-gradient-to-r from-primary to-secondary h-4 rounded-full"
-              style={{ width: `${perc}%` }}
-              ></div>
+                <div
+                className="bg-white h-4 rounded-full"
+                style={{ width: `${perc}%` }}
+                ></div>
+                <span className="absolute left-1/2 transform -translate-x-1/2 mt-1 text-white font-bold">
+                  {perc}%
+                </span>
             </div>
         </motion.div>
       </section>
