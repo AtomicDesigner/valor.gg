1aa2e55054efaaa91f688ef62969846f4c0b2da9
AtomicDesigner <ifeelcooler@gmail.com>
2025-03-12 03:50:46 +0100

    emptied updates

--- src/pages/updates.tsx
+++ src/pages/updates.tsx
@@ -1,12 +1,6 @@
 import { useInView } from 'react-intersection-observer';
 
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/sections/index/Hero";
-import About from "@/components/sections/index/About";
-import Experience from "@/components/sections/index/Experience";
-import Projects from "@/components/sections/index/Projects";
-import Footer from "@/components/sections/index/Footer";
-import Loading from "@/components/sections/loading";
 import { GridPattern } from "@/components/GridPattern";
 
 export default function Home() {
@@ -27,12 +21,6 @@ export default function Home() {
           y={-1}
           className='z-[-5]'
         />
-        <Hero inView={inView} descRef={ref} />
-        <About />
-        <Experience />
-        <Projects />
-        <Footer />
-        <Loading />
       </main>
     </>
   );
