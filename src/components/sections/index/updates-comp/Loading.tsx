2e2de8dc2f7621825f4e04f0a95851cae8edd4b6
AtomicDesigner <ifeelcooler@gmail.com>
2025-03-12 04:31:34 +0100

    test again

--- src/components/sections/index/updates-comp/Loading.tsx
+++ src/components/sections/index/updates-comp/Loading.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Hero({ inView, descRef }: { inView: boolean, descRef: any }) {
   return (
     <>
-      <section className="max-w-4xl w-full flex flex-col mx-auto pt-44">
+      <section className="max-w-4xl w-full flex flex-col mx-auto pt-20 md:pt-32 lg:pt-44">
         <motion.div
           className="relative sm:p-12 p-6 w-full flex flex-col bg-gradient-to-br from-primary to to-secondary rounded-lg border-1 border-accent shadow-2xl shadow-background"
           initial={{ transform: 'translateY(30px)', opacity: 0 }}
@@ -13,13 +13,13 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
           ref={descRef}
         >
           <h2 className="font-bold lg:text-7xl md:text-6xl sm:text-5xl text-4xl">
-            Next bot update status:
+        Next bot update status:
           </h2>
-          <div className="mt-6 w-full bg-gray-200 rounded-full h-4 dark:bg-gray-700">
-            <div
-              className="bg-primary h-4 rounded-full"
-              style={{ width: `${inView ? '75%' : '0%'}` }}
-            ></div>
+          <div className="mt-6 w-full bg-gray-700 rounded-full h-4 dark:bg-gray-900">
+        <div
+          className="bg-white h-4 rounded-full"
+          style={{ width: `${inView ? '75%' : '0%'}` }}
+        ></div>
           </div>
         </motion.div>
       </section>
