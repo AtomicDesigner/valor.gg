3cee43eda2292b87d538c91155b25efcf5a92286
AtomicDesigner <ifeelcooler@gmail.com>
2025-03-12 17:56:57 +0100

    re-aliogned

--- src/components/sections/index/updates-comp/AnimatedChecklist.tsx
+++ src/components/sections/index/updates-comp/AnimatedChecklist.tsx
@@ -17,19 +17,19 @@ const AnimatedChecklist = () => {
     }, []);
 
     return (
-        <section className="max-w-4xl w-full flex flex-col mx-auto pt-10 absolute top-32 left-0 right-0">
+        <section className="max-w-4xl w-full flex flex-col mx-auto pt-10 absolute top-31 left-0 right-0">
             <div className="flex flex-col space-y-2 p-4">
+            <h2 className="font-bold lg:text-7xl md:text-6xl sm:text-5xl text-4xl">
+                    To Do:
+                </h2>
                 {checkedItems.map((checked, index) => (
                 <motion.div
                     key={index}
                     initial={{ opacity: 0, x: -50 }}
                     animate={{ opacity: 1, x: 0 }}
-                    transition={{ delay: index * 0.2 }}
+                    transition={{ delay: index * 0.1 }}
                     className="flex items-center space-x-3 p-2 bg-gray-200 dark:bg-gray-800 rounded-lg"
                 >
-                <h2 className="font-bold lg:text-7xl md:text-6xl sm:text-5xl text-4xl">
-                    To Do:
-                </h2>
                 <div className="w-5 h-5 flex items-center justify-center border-2 border-gray-600 dark:border-gray-400 rounded-md">
                     {checked && <span className="text-green-500">✔</span>}
                 </div>
